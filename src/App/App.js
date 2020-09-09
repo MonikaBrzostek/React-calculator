1bb4eaa03f54f2e24cf6232b6cd830fcf13b6e52
Monika Brzostek <brzostek.monika@gmail.com>
2020-09-09 11:23:12 +0200

    Added checker for dividing by 0 and rounding to 5 decimals

--- src/App/App.js
+++ src/App/App.js
@@ -4,9 +4,6 @@ import Result from '../Result/Result'
 import Keypad from '../Keypad/Keypad'
 import mathloop from './video/mathloop.mp4'
 
-// dzielenie przez 0
-//
-
 class App extends React.Component {
   constructor() {
     super()
@@ -15,38 +12,37 @@ class App extends React.Component {
     }
   }
 
-  // warning about eval
-  // jak inaczej rozwiazac zerowanie wyniku
   handleClick = (e) => {
     if (e.target.name !== '=' && e.target.name !== 'C') {
       let result = this.state.result + e.target.name
       this.setState({ result })
+    } else if (
+      //checker for deviding by zero
+      e.target.name === '=' &&
+      this.state.result.includes('/0') === true
+    ) {
+      alert("You can't divide by 0!!!")
+      this.setState({ result: '' })
     } else if (e.target.name === '=') {
-      let result = eval(this.state.result)
-      this.setState({ result })
+      //result count + rounding to 5 decimals
+      let result = eval(this.state.result).toFixed(5)
+      console.log(result)
+      if (result.includes('.00000')) {
+        result = Math.round(result)
+        this.setState({ result })
+      } else {
+        this.setState({ result })
+      }
     } else if (e.target.name === 'C') {
       this.setState({ result: '' })
     }
+    console.log(this.state.result)
   }
 
   render() {
     return (
       <div>
-        <video
-          autoPlay
-          loop
-          muted
-          style={{
-            position: 'absolute',
-            width: '100%',
-            left: '50%',
-            top: '50%',
-            height: '1000',
-            objectFit: 'cover',
-            transform: 'translate(-50%, -50%)',
-            zIndex: '-1',
-          }}
-        >
+        <video autoPlay loop muted className="video">
           <source src={mathloop} type="video/mp4" />
         </video>
         <div className="calculator">
