411de71a0b9c66c11dab802597723a496037df56
Monika Brzostek <brzostek.monika@gmail.com>
2020-09-23 11:40:59 +0200

    Improved code quality and added blocking of double dots in equation

--- src/App/App.js
+++ src/App/App.js
@@ -13,101 +13,104 @@ class App extends React.Component {
   }
 
   handleClick = (e) => {
-    const result = this.state.result
-    if (
-      e.target.name !== '=' &&
-      e.target.name !== 'C' &&
-      this.state.resultEvaluated === false
-    ) {
-      if (
-        //checker for not starting with a mathematical symbol
-        this.state.result === '' &&
-        (e.target.name === '*' ||
-          e.target.name === '/' ||
-          e.target.name === '-' ||
-          e.target.name === '+' ||
-          e.target.name === '.')
-      ) {
+    const char = e.target.name
+    if (char !== '=' && char !== 'C' && this.state.resultEvaluated === false) {
+      //checker for not starting with a mathematical symbol
+      if (this.state.result === '' && this.isMathematicalSymbol(char)) {
         this.setState({ result: '', resultEvaluated: false })
-      } else if (
-        //checker for not starting with double zero
-        this.state.result === '0' &&
-        (e.target.name === '0' ||
-          e.target.name === '1' ||
-          e.target.name === '2' ||
-          e.target.name === '3' ||
-          e.target.name === '4' ||
-          e.target.name === '5' ||
-          e.target.name === '6' ||
-          e.target.name === '7' ||
-          e.target.name === '8' ||
-          e.target.name === '9')
+        return
+      }
+
+      //checker for not starting with double zero
+      if (this.state.result === '0' && this.isNumber(char)) {
+        this.setState({ result: char, resultEvaluated: false })
+        return
+      }
+
+      //checker for no setting math symbols after eachother
+      if (
+        this.isMathematicalSymbol(this.getLastChar(this.state.result)) &&
+        this.isMathematicalSymbol(char)
       ) {
-        this.setState({ result: e.target.name, resultEvaluated: false })
-      } else if (
-        //checker for no setting math symbols after eachother
-        (result.slice(result.length - 1) === '/' ||
-          result.slice(result.length - 1) === '*' ||
-          result.slice(result.length - 1) === '+' ||
-          result.slice(result.length - 1) === '-' ||
-          result.slice(result.length - 1) === '.') &&
-        (e.target.name === '/' ||
-          e.target.name === '*' ||
-          e.target.name === '-' ||
-          e.target.name === '+' ||
-          e.target.name === '.')
-      ) {
-        console.log('Too many mathematical symbols in a row')
-      } else if (this.state.result.length > 12) {
-        //checker for a too long number
+        const result = this.state.result.slice(0, -1) + char
+        this.setState({ result })
+        return
+      }
+
+      //blocking using double dots
+      if (this.state.result.includes('.') && char === '.') {
+        return
+      }
+
+      //checker for a too long number
+      if (this.state.result.length > 12) {
         alert('The number is too long for our tiny calculator :(')
         this.setState({ result: '' })
-      } else {
-        const result = this.state.result + e.target.name
-        this.setState({ result, resultEvaluated: false })
+        return
       }
-    } else if (this.state.resultEvaluated === true && e.target.name === '=') {
+      this.setState({
+        result: this.state.result + char,
+        resultEvaluated: false,
+      })
+      return
+    }
+
+    // blocking double evaluation of result
+    if (this.state.resultEvaluated === true && char === '=') {
       this.clear()
-    } else if (
-      //checker for deviding by zero
-      e.target.name === '=' &&
-      this.state.result.includes('/0') === true
-    ) {
+      return
+    }
+
+    //checker for deviding by zero
+    if (char === '=' && this.state.result.includes('/0') === true) {
       alert("You can't divide by 0!!!")
       this.clear()
-    } else if (
-      //resetting the result after evaluation
-      this.state.resultEvaluated === true &&
-      (e.target.name === '0' ||
-        e.target.name === '1' ||
-        e.target.name === '2' ||
-        e.target.name === '3' ||
-        e.target.name === '4' ||
-        e.target.name === '5' ||
-        e.target.name === '6' ||
-        e.target.name === '7' ||
-        e.target.name === '8' ||
-        e.target.name === '9')
-    ) {
-      this.setState({ result: e.target.name, resultEvaluated: false })
-    } else if (
-      //allowing to add math symbol after the result
-      (e.target.name === '*' ||
-        e.target.name === '/' ||
-        e.target.name === '-' ||
-        e.target.name === '+' ||
-        e.target.name === '+') &&
+      return
+    }
+
+    //resetting the result after evaluation
+    if (this.state.resultEvaluated === true && this.isNumber(char)) {
+      this.setState({ result: char, resultEvaluated: false })
+      return
+    }
+
+    //allowing to add math symbol after the result
+    if (
+      this.isMathematicalSymbol(char) &&
       this.state.resultEvaluated === true
     ) {
-      const result = this.state.result + e.target.name
-      this.setState({ result, resultEvaluated: false })
-    } else if (e.target.name === '=') {
-      //result count + rounding to 5 decimals
-      this.calculateResult()
-    } else if (e.target.name === 'C') {
-      this.clear()
+      this.setState({
+        result: this.state.result + char,
+        resultEvaluated: false,
+      })
+      return
     }
-    // console.log(this.state.result)
+
+    //result count + rounding to 5 decimals
+    if (char === '=') {
+      this.calculateResult()
+      return
+    }
+
+    if (char === 'C') {
+      this.clear()
+      return
+    }
+  }
+
+  getLastChar(text) {
+    return text.slice(text.length - 1)
+  }
+
+  isMathematicalSymbol(char) {
+    const mathematicalSymbols = ['*', '/', '-', '+', '-', '.']
+    if (mathematicalSymbols.includes(char)) return true
+    else return false
+  }
+
+  isNumber(char) {
+    if (!isNaN(char)) return true
+    else return false
   }
 
   numberOfDecimals(number) {
@@ -120,13 +123,12 @@ class App extends React.Component {
     const result = this.state.result
     if (result === '') {
       console.log('The result is empty')
-    } else if (
-      result.slice(result.length - 1) === '/' ||
-      result.slice(result.length - 1) === '*' ||
-      result.slice(result.length - 1) === '+' ||
-      result.slice(result.length - 1) === '-'
-    ) {
+      return
+    }
+
+    if (this.isMathematicalSymbol(this.getLastChar(this.state.result))) {
       alert('You forgot to finish the equation!')
+      return
     } else {
       let result = eval(this.state.result)
       if (this.numberOfDecimals(result) > 5) {
